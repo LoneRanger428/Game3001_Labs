19010a0b1d11100ef1a3451281455900ee57ca60
Mohd_Saad <148091167+LoneRanger428@users.noreply.github.com>
2024-02-20 13:48:34 -0500

    Click Drag Script Updated and Path Manager  Connect

--- GAME3001_Lab5_Start_Project/GAME3001_Lab5_Start/Assets/_MyAssets/_Scripts/GridManager.cs
+++ GAME3001_Lab5_Start_Project/GAME3001_Lab5_Start/Assets/_MyAssets/_Scripts/GridManager.cs
@@ -9,7 +9,8 @@ public enum TileStatus
     CLOSED,
     IMPASSABLE,
     GOAL,
-    START
+    START,
+    PATH
 };
 
 public enum NeighbourTile
@@ -83,6 +84,26 @@ public class GridManager : MonoBehaviour
             }
             mines.Clear();
         }
+
+        if(Input.GetKeyDown(KeyCode.F)) // Start PathFinding
+        {
+            //Get ship node 
+            GameObject ship = GameObject.FindGameObjectWithTag("Ship");
+            Vector2 shipIndices = ship.GetComponent<NavigationObject>().GetGridIndex();
+            PathNode start = grid[(int)shipIndices.y, (int)shipIndices.x].GetComponent<TileScript>().Node;
+            //Get Goal Node
+            GameObject planet = GameObject.FindGameObjectWithTag("Planet");
+            Vector2 planetIndices = planet.GetComponent<NavigationObject>().GetGridIndex();
+            PathNode goal = grid[(int)planetIndices.x, (int)planetIndices.y].GetComponent<TileScript>().Node;
+
+            //Start the algorithm 
+            PathManager.Instance.GetShortestPath(start, goal);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)) // Reset grid/pathfinding 
+        {
+            SetTileStatuses();
+        }
     }
 
     private void BuildGrid()
@@ -107,6 +128,8 @@ public class GridManager : MonoBehaviour
                 panelTransform.localScale = Vector3.one;
                 panelTransform.anchoredPosition = new Vector3(64f * col, -64f * row);
                 tileScript.tilePanel = panelInst.GetComponent<TilePanelScript>();
+                //Create a new PathNode for the new tile.
+                tileScript.Node = new PathNode(tileInst);
             }
             count--;
         }
@@ -121,28 +144,58 @@ public class GridManager : MonoBehaviour
         SetTileCosts(planetIndices);
     }
 
-    private void ConnectGrid()
+    public void ConnectGrid()
     {
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < columns; col++)
             {
                 TileScript tileScript = grid[row, col].GetComponent<TileScript>();
+                tileScript.ResetNeighbourConnections();
+                if(tileScript.status == TileStatus.IMPASSABLE)
+                {
+                    if (!(grid[row-1,col].GetComponent<TileScript>().status == TileStatus.IMPASSABLE))
+                    {
+                        tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row-1,col]);
+                        tileScript.Node.AddConnection(new PathConnection(tileScript.Node, grid[row-1,col].GetComponent<TileScript>().Node
+                            , Vector3.Distance(tileScript.transform.position, grid[row-1,col].transform.position)));
+                    }
+                }
                 if (row > 0) // Set top neighbour if tile is not in top row.
                 {
-                    tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row - 1, col]);
+                    if (!(grid[row - 1, col].GetComponent<TileScript>().status == TileStatus.IMPASSABLE))
+                    {
+                        tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row, col +1]);
+                        tileScript.Node.AddConnection(new PathConnection(tileScript.Node, grid[row, col +1].GetComponent<TileScript>().Node
+                            , Vector3.Distance(tileScript.transform.position, grid[row, col +1].transform.position)));
+                    }
                 }
                 if (col < columns - 1) // Set right neighbour if tile is not in rightmost row.
                 {
-                    tileScript.SetNeighbourTile((int)NeighbourTile.RIGHT_TILE, grid[row, col + 1]);
+                    if (!(grid[row, col +1].GetComponent<TileScript>().status == TileStatus.IMPASSABLE))
+                    {
+                        tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row +1, col]);
+                        tileScript.Node.AddConnection(new PathConnection(tileScript.Node, grid[row +1, col].GetComponent<TileScript>().Node
+                            , Vector3.Distance(tileScript.transform.position, grid[row +1, col].transform.position)));
+                    }
                 }
                 if (row < rows - 1) // Set bottom neighbour if tile is not in bottom row.
                 {
-                    tileScript.SetNeighbourTile((int)NeighbourTile.BOTTOM_TILE, grid[row + 1, col]);
+                    if (!(grid[row +1, col].GetComponent<TileScript>().status == TileStatus.IMPASSABLE))
+                    {
+                        tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row +1, col]);
+                        tileScript.Node.AddConnection(new PathConnection(tileScript.Node, grid[row +1, col].GetComponent<TileScript>().Node
+                            , Vector3.Distance(tileScript.transform.position, grid[row +1, col].transform.position)));
+                    }
                 }
                 if (col > 0) // Set left neighbour if tile is not in leftmost row.
                 {
-                    tileScript.SetNeighbourTile((int)NeighbourTile.LEFT_TILE, grid[row, col - 1]);
+                    if (!(grid[row, col-1].GetComponent<TileScript>().status == TileStatus.IMPASSABLE))
+                    {
+                        tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row, col -1]);
+                        tileScript.Node.AddConnection(new PathConnection(tileScript.Node, grid[row, col -1].GetComponent<TileScript>().Node
+                            , Vector3.Distance(tileScript.transform.position, grid[row, col -1].transform.position)));
+                    }
                 }
             }
         }
@@ -187,5 +240,27 @@ public class GridManager : MonoBehaviour
                 tileScript.tilePanel.costText.text = tileScript.cost.ToString("F1");
             }
         }
+    }
+
+    public void SetTileStatuses()
+    {
+        foreach(GameObject go in grid)
+        {
+            go.GetComponent<TileScript>().SetStatus(TileStatus.UNVISITED);
+        }
+        foreach(GameObject mine in grid)
+        {
+            Vector2 mineIndex = mine.GetComponent<NavigationObject>().GetGridIndex();
+            grid[(int)mineIndex.y, (int)mineIndex.x].GetComponent<TileScript>().SetStatus(TileStatus.IMPASSABLE);
+        }
+
+        //Set the title under the ship to start
+        GameObject ship = GameObject.FindGameObjectWithTag("Ship");
+        Vector2 shipIndices = ship.GetComponent<NavigationObject>().GetGridIndex();
+        grid[(int)shipIndices.y, (int)shipIndices.x].GetComponent<TileScript>().SetStatus(TileStatus.START);
+        //Set the tile under the planet to goal
+        GameObject planet = GameObject.FindGameObjectWithTag("Planet");
+        Vector2 planetIndices = planet.GetComponent<NavigationObject>().GetGridIndex();
+        grid[(int)planetIndices.y, (int)planetIndices.x].GetComponent<TileScript>().SetStatus(TileStatus.GOAL);
     }
 }
--- GAME3001_Lab5_Start_Project/GAME3001_Lab5_Start/Assets/_MyAssets/_Scripts/PathManager.cs
+++ GAME3001_Lab5_Start_Project/GAME3001_Lab5_Start/Assets/_MyAssets/_Scripts/PathManager.cs
@@ -1,108 +1,113 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class PathNode
-{
-    public GameObject Tile {  get; private set; }
-    public List<PathConnection> connection;
-
-    public PathNode(GameObject tile)
+    public class PathNode
     {
-        Tile = tile;
-        connection = new List<PathConnection>();
-    }
+        public GameObject Tile {  get; private set; }
+        public List<PathConnection> connection;
 
-    public void AddConnection(PathConnection c)
-    {
-        connection.Add(c);
-    }
-}
-
-public class PathConnection
-{
-    public float Cost { get; set; }
-    public PathNode FromNode { get; private set; }
-    public PathNode ToNode { get; private set; }
-
-    public PathConnection(PathNode from, PathNode to, float cost = 1f)
-    {
-        FromNode = from;
-        ToNode = to;
-        Cost = cost;
-    }
-}
-
-public class NodeRecord
-{
-    public PathNode Node { get; set; }
-    public NodeRecord FronRecord { get; set; }
-    public PathConnection PathConnection { get; set; }
-    public float CostSoFar { get; set; }
-}
-
-[System.Serializable]
-public class PathManager : MonoBehaviour
-{
-    public List<NodeRecord> openlist;
-    public List<NodeRecord> closeList;
-
-    public List<PathConnection> path; // What will be the shortest path
-
-    public static PathManager Instance { get; private set; } // Static object of the Class
-
-    private void Awake()
-    {
-        if (Instance == null) // If the object/instance doesn;t exist yet
+        public PathNode(GameObject tile)
         {
-            Instance = this;
+            Tile = tile;
+            connection = new List<PathConnection>();
         }
-        else
+
+        public void AddConnection(PathConnection c)
         {
-            Destroy(gameObject);
+            connection.Add(c);
         }
     }
 
-    private void Initialize()
+    public class PathConnection
     {
-        openlist = new List<NodeRecord>();
-        closeList = new List<NodeRecord>();
-        path = new List<PathConnection>();
+        public float Cost { get; set; }
+        public PathNode FromNode { get; private set; }
+        public PathNode ToNode { get; private set; }
+
+        public PathConnection(PathNode from, PathNode to, float cost = 1f)
+        {
+            FromNode = from;
+            ToNode = to;
+            Cost = cost;
+        }
     }
 
-    public NodeRecord GetSmallestNode()
+    public class NodeRecord
     {
-        NodeRecord smallestNode = openlist[0];
-        for(int i = 1; i < openlist.Count; i++)
+        public PathNode Node { get; set; }
+        public NodeRecord FronRecord { get; set; }
+        public PathConnection PathConnection { get; set; }
+        public float CostSoFar { get; set; }
+    }
+
+    [System.Serializable]
+    public class PathManager : MonoBehaviour
+    {
+        public List<NodeRecord> openlist;
+        public List<NodeRecord> closeList;
+
+        public List<PathConnection> path; // What will be the shortest path
+
+        public static PathManager Instance { get; private set; } // Static object of the Class
+
+        private void Awake()
         {
-            if (openlist[i].CostSoFar < smallestNode.CostSoFar)
+            if (Instance == null) // If the object/instance doesn;t exist yet
             {
-                smallestNode = openlist[i];
+                Instance = this;
             }
-            // If they're the same, fllip a coin
-            else if (openlist[i].CostSoFar == smallestNode.CostSoFar)
+            else
             {
-                smallestNode = (Random.value < 0.5f ? openlist[i] : smallestNode);
+                Destroy(gameObject);
             }
         }
-        return smallestNode; //Return the ModeRecord with the smallest CostSoFar
-    }
 
-    public bool ConstainsNode(List<NodeRecord> list, PathNode node)
-    {
-        foreach(NodeRecord record in list)
+        private void Initialize()
         {
-            if (record.Node == node) return true;
+            openlist = new List<NodeRecord>();
+            closeList = new List<NodeRecord>();
+            path = new List<PathConnection>();
         }
-        return false;
-    }
 
-    public NodeRecord GetNodeRecord(List<NodeRecord> list, PathNode node)
-    {
-        foreach(NodeRecord record in list)
+        public void GetShortestPath(PathNode start, PathNode end)
         {
-            if(record.Node == node) return record;
+
         }
-        return null;
+
+        public NodeRecord GetSmallestNode()
+        {
+            NodeRecord smallestNode = openlist[0];
+            for(int i = 1; i < openlist.Count; i++)
+            {
+                if (openlist[i].CostSoFar < smallestNode.CostSoFar)
+                {
+                    smallestNode = openlist[i];
+                }
+                // If they're the same, fllip a coin
+                else if (openlist[i].CostSoFar == smallestNode.CostSoFar)
+                {
+                    smallestNode = (Random.value < 0.5f ? openlist[i] : smallestNode);
+                }
+            }
+            return smallestNode; //Return the ModeRecord with the smallest CostSoFar
+        }
+
+        public bool ConstainsNode(List<NodeRecord> list, PathNode node)
+        {
+            foreach(NodeRecord record in list)
+            {
+                if (record.Node == node) return true;
+            }
+            return false;
+        }
+
+        public NodeRecord GetNodeRecord(List<NodeRecord> list, PathNode node)
+        {
+            foreach(NodeRecord record in list)
+            {
+                if(record.Node == node) return record;
+            }
+            return null;
+        }
     }
-}
